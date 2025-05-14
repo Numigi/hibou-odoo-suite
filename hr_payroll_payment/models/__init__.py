9df747f816cf428ee1ecc49b1ebe618bf91a392b
Rivo Lalaina <rivo2302@gmail.com>
2025-05-14 14:01:05 +0300

    [16.0][ADD] hr_payroll_payment

---
+++ hr_payroll_payment/models/__init__.py
@@ -0,0 +1,6 @@
+# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
+from . import account_journal
+from . import hr_contract
+from . import hr_payslip
+from . import hr_payslip_patch
